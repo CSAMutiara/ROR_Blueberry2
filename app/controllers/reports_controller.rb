350bf2e92ce9b5c5b87d40b99a6fd528af6d0573
Ayesha Mutiara <a100017@aup.edu>
2018-04-08 23:24:52 +0000

    started over with reports first this time, reports can be created, updated and destroyed

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,49 @@
+class ReportsController < ApplicationController
+    before_action :find_report, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        @reports = Report.all.order("created_at DESC")
+    end
+    
+    def show
+    end
+    
+    def new
+        @report = Report.new
+    end
+    
+    def create
+        @report = Report.new(report_params)
+        if @report.save
+            redirect_to @report
+        else 
+            render 'new'
+        end 
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @report.update(report_params)
+            redirect_to @report
+        else 
+            render 'edit'
+        end 
+    end 
+    
+    def destroy
+        @report.destroy
+        redirect_to root_path
+    end 
+    
+    private
+    
+    def report_params
+        params.require(:report).permit(:year)
+    end 
+    
+    def find_report
+        @report = Report.find(params[:id])
+    end 
+end
