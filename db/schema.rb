ad44c8d37eb7ed93ee26b5ab9f41839a20150a64
Ayesha Mutiara <a100017@aup.edu>
2018-04-10 17:17:51 +0000

    teaching link in yearly report cards work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409010102) do
+ActiveRecord::Schema.define(version: 20180410115532) do
 
   create_table "iactivities", force: :cascade do |t|
     t.string "name"
@@ -87,6 +87,12 @@ ActiveRecord::Schema.define(version: 20180409010102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["report_id"], name: "index_tactivities_on_report_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
