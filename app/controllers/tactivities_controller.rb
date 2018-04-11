82f21578389ae2fac8bb7248bbb84f1900eefeb6
Ayesha Mutiara <a100017@aup.edu>
2018-04-11 01:43:44 +0000

    (for tactivities) pretty much all links redirect/render properly... can update and delete too

--- app/controllers/tactivities_controller.rb
+++ app/controllers/tactivities_controller.rb
@@ -1,5 +1,6 @@
 class TactivitiesController < ApplicationController
-    before_action :find_tactivity, only: [:show]
+    before_action :find_tactivity, only: [:show, :edit, :update, :destroy]
+    before_action :find_report, only: [:create, :destroy]
     
     def index
         @tactivities = Tactivity.all.order("created_at DESC")
@@ -13,24 +14,39 @@ class TactivitiesController < ApplicationController
     end
     
     def create
-        @report = Report.find(params[:report_id])
-        @tactivity = @report.tactivities.create(params[:tactivity].permit(:date, :name, :ta1, :ta2, :ta3, :ta4, :status))
-        redirect_to report_path(@report)
+        @tactivity = @report.tactivities.create(tactivity_params)
+        render 'tactivities/show'
     end
     
     def edit
+        @tactivities = Tactivity.all.order("created_at DESC")
     end
     
     def update
+        if @tactivity.update(tactivity_params)
+            redirect_to report_tactivity_path
+        else
+            render 'edit'
+        end
     end
     
     def destroy
+        @tactivity.destroy
+        redirect_to report_path(@report)
     end
     
     private
     
+    def tactivity_params
+        params.require(:tactivity).permit(:date, :name, :ta1, :ta2, :ta3, :ta4, :status)
+    end 
+    
     def find_tactivity
         @tactivity = Tactivity.find(params[:id])
+    end
+    
+    def find_report
+        @report = Report.find(params[:report_id])
     end
     
 end
