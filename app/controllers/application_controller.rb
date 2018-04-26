2199da119c9a5db0c2585e918a7432daec919ddd
Salim Huneidi <salim@huneidi.com>
2018-04-26 09:48:27 +0000

    new login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :set_current_user
+  before_action :set_current_user
   protected # prevents method from being invoked by a route
   def set_current_user
     # we exploit the fact that the below query may return nil
