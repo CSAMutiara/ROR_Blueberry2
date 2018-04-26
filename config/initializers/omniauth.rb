58926b9f0ff13c4fe1f6c0d6c21ee5b88778c510
Salim Huneidi <salim@huneidi.com>
2018-04-26 12:53:04 +0000

    Omniauth changed to not use ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['NGnzk6p0a3GoFwsstwUqFe0EK'], ENV["j7Vzwz85ailklahUW74ZeUPK8zNkqpjH8A2kgY6JTdobExFfqy"]
+  provider :twitter, 'NGnzk6p0a3GoFwsstwUqFe0EK', 'j7Vzwz85ailklahUW74ZeUPK8zNkqpjH8A2kgY6JTdobExFfqy'
 end
