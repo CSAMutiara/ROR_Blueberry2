9d465759c496d82bd8abac7db1dd5904c143b7af
Salim Huneidi <salim@huneidi.com>
2018-05-13 22:38:46 +0000

    schema gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410115532) do
+ActiveRecord::Schema.define(version: 20180513162030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20180410115532) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["report_id"], name: "index_iactivities_on_report_id"
+  end
+
+  create_table "references", force: :cascade do |t|
+    t.string "type"
+    t.date "date"
+    t.integer "report_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reports", force: :cascade do |t|
